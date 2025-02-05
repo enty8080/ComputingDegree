a254531f9639190efee66004e701fad995323932
Ivan Nikolskiy <enty8080@gmail.com>
2025-02-05 20:01:27 +0000

    Create main.c

---
+++ main.c
@@ -0,0 +1,2 @@
+#include<stdio.h>/*Name:IvanGroup:ADate:2020Jlab:10*/
+int main(void){/*forloop*/for(int i=0;/*condichii*/i<10;++i){if(i%2==0/*isevendb?*/){printf("%d\n",i)/*output*/;}continue/*ensure100%loopcontinuationen*/;}/*exit*/return 0;}/*end*/
